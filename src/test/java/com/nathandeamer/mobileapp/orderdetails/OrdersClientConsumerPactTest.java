a74c7d48353849dd97674f46c2facd2efe1564e2
Nathan Deamer <nathan.deamer@leisurepassgroup.com>
2021-07-19 09:48:05 +0100

    Tidy up

--- src/test/java/com/nathandeamer/mobileapp/orderdetails/OrdersClientConsumerPactTest.java
+++ src/test/java/com/nathandeamer/mobileapp/orderdetails/OrdersClientConsumerPactTest.java
@@ -1,11 +1,13 @@
 package com.nathandeamer.mobileapp.orderdetails;
 
+import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
+
 import com.google.common.collect.ImmutableList;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
