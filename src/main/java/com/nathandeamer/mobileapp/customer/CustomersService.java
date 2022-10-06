d073a2ea11487a4a669d07a85579a1faf483383e
nathandeamer <nd@nathandeamer.com>
2022-10-06 14:55:17 +0100

    Fixed compile error

--- src/main/java/com/nathandeamer/mobileapp/customer/CustomersService.java
+++ src/main/java/com/nathandeamer/mobileapp/customer/CustomersService.java
@@ -3,13 +3,15 @@ package com.nathandeamer.mobileapp.customer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class CustomersService {
 
   private final CustomersClient customersClient;
 
-  public CustomerResponse getCustomer(int customerId) {
+  public CustomerResponse getCustomer(UUID customerId) {
       return customersClient.getCustomer(customerId);
   }
 
